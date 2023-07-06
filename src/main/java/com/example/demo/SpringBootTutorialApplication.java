d079acf94fe252fe43bf6835d7df575681dcd3fc
AndrasFejes22 <afejes22@gmail.com>
2023-07-06 23:30:36 +0200

    main updated, banner added

--- src/main/java/com/example/demo/SpringBootTutorialApplication.java
+++ src/main/java/com/example/demo/SpringBootTutorialApplication.java
@@ -3,8 +3,13 @@ package com.example.demo;
 import com.example.demo.model.UserContext;
 import com.example.demo.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.annotation.SessionScope;
 
@@ -17,10 +22,23 @@ import java.util.Optional;
 @SpringBootApplication
 public class SpringBootTutorialApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringBootTutorialApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTutorialApplication.class, args);
+		//SpringApplication.run(SpringBootTutorialApplication.class, args);
+		new SpringApplicationBuilder()
+				.sources(SpringBootTutorialApplication.class)
+				.bannerMode(Banner.Mode.OFF)
+				.lazyInitialization(true)
+				.listeners(event -> logEvent(event))
+				.run(args);
 	}
 
+	private static void logEvent(ApplicationEvent event) {
+		LOGGER.info("*** event: {}", event.getClass().getSimpleName());
+	}
+
+	// kissebb projektetknél lehet itt, amúgy külön kezelendő
 	@Bean
 	@SessionScope
 	// for demo purposes only!
