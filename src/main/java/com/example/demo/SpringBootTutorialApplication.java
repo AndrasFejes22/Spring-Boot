8a64011a6f3dfed8c921531b202c76759db3d20e
AndrasFejes22 <afejes22@gmail.com>
2023-07-08 16:07:18 +0200

    arguments

--- src/main/java/com/example/demo/SpringBootTutorialApplication.java
+++ src/main/java/com/example/demo/SpringBootTutorialApplication.java
@@ -5,6 +5,7 @@ import com.example.demo.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import org.springframework.context.ApplicationEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -36,6 +38,19 @@ public class SpringBootTutorialApplication {
 
 	private static void logEvent(ApplicationEvent event) {
 		LOGGER.info("*** event: {}", event.getClass().getSimpleName());
+	}
+
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return arguments -> {
+			LOGGER.info("Raw arguments: {}", (Object) arguments.getSourceArgs());
+			LOGGER.info("Option names: {}", arguments.getOptionNames());
+			if (arguments.containsOption("hello")) {
+				List<String> helloArguments = arguments.getOptionValues("hello");
+				LOGGER.info("Hello {}", helloArguments);
+			}
+			LOGGER.info("Non-options: {}", arguments.getNonOptionArgs());
+		};
 	}
 
 	// kissebb projektetknél lehet itt, amúgy külön kezelendő
